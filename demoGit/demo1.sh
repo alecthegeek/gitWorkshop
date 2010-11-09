15caa68e25871a32ccd5783abecd61037cd9037c
Alec Clews <alec.clews@voga.com.au>
2010-11-10 08:07:49 +1100

    Added demo scripts and updated license in readme

---
+++ demoGit/demo1.sh
@@ -0,0 +1,109 @@
+#! /bin/sh 
+
+cd demoGitBranchMerge
+
+[ -d .git ] && rm -rf .git
+for i in .gitignore text1 text2 ; do
+	[ -f $i ] && rm -f $i
+done
+
+clear
+set -v
+# create a demo Git repository
+git init
+read x
+
+# "Create some data files"
+echo 1 > text1
+echo 1 > text2
+
+# Background housekeeping chore -- set up .gitignore file
+echo *~ > .gitignore
+echo *.swp >> .gitignore
+
+head .gitignore
+
+# Now add new files to index
+git add text1 text2 .gitignore
+read x
+
+# and make the first Git commit
+git commit -m "Initial commit on master branch"
+read x
+
+git tag "C1"
+read x
+
+# "Pretend to do more work"
+echo 2 >> text1
+echo 2 >> text2
+
+# NB -a adds updates to *existing* files into index without seperate add
+git commit -am "Second commit on master branch"
+read x
+
+git log --pretty=oneline --abbrev-commit --all --graph
+read x
+
+# Move back to first commit
+git checkout C1
+
+# Create a new branch for some work
+git branch branch1
+
+# Move the working copy to the new branch
+git checkout branch1
+
+# Pretend to do more work
+echo b1 >> text1
+echo b1 >> text2
+
+# New work committed on branch
+git commit -am "First commit on branch1"
+read x
+
+git log --pretty=oneline --abbrev-commit --all --graph
+read x
+
+# "Pretend to do more work"
+echo b2 >> text1
+echo b2 >> text2
+
+git commit -am "Second commit on branch1"
+
+git log --pretty=oneline --abbrev-commit --all --graph
+read x
+
+# Now move back to master branch
+git checkout master
+read x
+
+# "Pretend to do more work"
+echo 3 >> text1
+echo 3 >> text2
+
+git commit -am "Third commit on master branch"
+read x
+
+git log --pretty=oneline --abbrev-commit --all --graph
+read x
+
+# Bring changes from branch1 onto master
+git merge branch1
+read x
+
+git status
+read x
+
+vim text1 text2
+
+# After resolve -- we are still in middle of merge -- we add and commit
+git add text1 text2
+git commit -m "Merged Master and branch1 on `date +%A`"
+read x
+
+git log --pretty=oneline --abbrev-commit --all --graph
+
+head text1 text2
+
+head text1 text1
---
+++ demoGit/demo2.sh
@@ -0,0 +1,103 @@
+#! /bin/sh 
+
+demoName=demoGitRebase
+
+[ -d $demoName ] || mkdir $demoName
+
+cd $demoName
+
+[ -d .git ] && rm -rf .git
+for i in .gitignore text1 text2 ; do
+	[ -f $i ] && rm -f $i
+done
+
+clear
+set -v
+# create a demo Git repository
+git init
+read x
+
+# "Create some data files"
+echo 1 > text1
+echo 1 > text2
+
+# Background housekeeping chore -- set up .gitignore file
+echo *~ > .gitignore
+echo *.swp >> .gitignore
+
+
+# Now add new files to index
+git add text1 text2 .gitignore
+
+# and make the first Git commit
+git commit -m "Initial commit on master branch"
+
+git tag "C1"
+
+# "Pretend to do more work"
+echo 2 >> text1
+
+# NB -a adds updates to *existing* files into index without seperate add
+git commit -am "Second commit on master branch"
+read x
+
+git log --pretty=oneline --abbrev-commit --all --graph
+read x
+
+# Move back to first commit
+git checkout C1
+
+# Create a new branch for some work
+git branch branch1
+
+# Move the working copy to the new branch
+git checkout branch1
+
+# Pretend to do more work
+echo b1 >> text2
+
+# New work committed on branch
+git commit -am "First commit on branch1"
+read x
+
+git log --pretty=oneline --abbrev-commit --all --graph
+read x
+
+# "Pretend to do more work"
+echo b2 >> text2
+
+git commit -am "Second commit on branch1"
+
+git log --pretty=oneline --abbrev-commit --all --graph
+read x
+
+# Now move back to master branch
+git checkout master
+read x
+
+# "Pretend to do more work"
+echo 3 >> text1
+
+git commit -am "Third commit on master branch"
+read x
+
+#git log --pretty=oneline --abbrev-commit --all --graph
+git k --all
+read x
+
+# Bring changes from master into branch work (catch up to master)
+
+# Move to branch1
+git checkout branch1
+
+# Rebase
+
+git rebase master
+
+#git log --pretty=oneline --abbrev-commit --all --graph
+git k --all
+read x
+git status
+read x
+
+head text1 text2
